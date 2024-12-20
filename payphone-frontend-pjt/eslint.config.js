56558b617b6b7d701d2771241dca520cb3fc34bc
Jiwoo <emily1290@naver.com>
2024-12-20 11:43:35 +0900

    fix: Typescript 관련 설정과 도구 제거

--- payphone-frontend-pjt/eslint.config.js
+++ payphone-frontend-pjt/eslint.config.js
@@ -1,34 +1,23 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue';
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{js,vue}'], // ts, tsx 관련 확장자 삭제
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
-  
+  ...pluginVue.configs['flat/essential'], // 기본 Vue 설정 유지
   {
     ...pluginCypress.configs.recommended,
     files: [
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
+      'cypress/e2e/**/*.{cy,spec}.{js,jsx}', // ts, tsx 관련 확장자 삭제
+      'cypress/support/**/*.{js,jsx}',
     ],
   },
   skipFormatting,
-]
+];
